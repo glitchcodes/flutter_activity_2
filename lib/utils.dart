7088a5c1eb6a58efe73cb7eec28bede37131cf41
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2025-03-01 21:02:49 +0800

    Update utils.dart

--- lib/utils.dart
+++ lib/utils.dart
@@ -4,9 +4,6 @@ class LoginException implements Exception {
   final Map<String, List<String>> errors;
 
   LoginException(this.errors);
-
-  // @override
-  // String toString() => "LoginException: $errors";
 }
 
 class Login {
@@ -15,41 +12,33 @@ class Login {
     'password': [],
   };
 
-  static void _throwError() {
-    throw LoginException(_errors);
-  }
-
   static bool authenticate(String email, String password) {
     _errors['email']!.clear();
     _errors['password']!.clear();
 
     if (email.isEmpty) {
       _errors['email']!.add('Required');
-      _throwError();
     }
 
     if (password.isEmpty) {
       _errors['password']!.add('Required');
-      _throwError();
     }
 
     if (!RegExp(r'^[^@]+@[^@]+\.[^@]+$').hasMatch(email)) {
       _errors['email']!.add('Invalid Email.');
-      _throwError();
     }
 
     switch (email) {
       case ('admin@account.root'):
         _errors['email']!.add('Warning. Reserved phrase.');
-        _throwError();
+        break;
       case ('superadmin@account.root'):
         _errors['email']!.add('Warning. Unused reserved phrase.');
-        _throwError();
+        break;
     }
 
     if (password.length < 8) {
       _errors['password']!.add('Password too short.');
-      _throwError();
     }
 
     if (!credentials.containsKey(email) || credentials[email] != password) {
@@ -57,7 +46,7 @@ class Login {
     }
 
     if (_errors['email']!.isNotEmpty || _errors['password']!.isNotEmpty) {
-      _throwError();
+      throw LoginException(_errors);
     }
 
     return true;
